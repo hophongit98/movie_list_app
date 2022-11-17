eed6ab87a3460ede1af263043ed3daf789b80630
Truong Ho Phong <hophongit98@gmail.com>
2022-11-17 22:24:59 +0700

    add movie model from DB, cloud

---
+++ app/src/main/java/com/example/movielistapp/model/Movie.kt
@@ -0,0 +1,24 @@
+package com.example.movielistapp.model
+
+/**
+ * Created by Phillip Truong
+ * date 17/11/2022.
+ */
+class Movie(
+    val id: String,
+    val imageUrl: String,
+    val movieName: String,
+    val duration: Int,
+    val kind: List<MovieType>,
+    val point: Float
+)
+
+enum class MovieType(type: String) {
+    ACTION("Action"),
+    ADVENTURE("Adventure"),
+    ANIMATION("Animation"),
+    COMEDY("Comedy"),
+    CRIME("Crime"),
+    DRAMA("Drama"),
+    SCIFI("Sci-Fi")
+}
